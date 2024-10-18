c9ccdfe067ee6671431ae2bdac2c1c63a81983d6
juyeon Bae <w2316@e-mirim.hs.kr>
2024-10-19 03:25:23 +0900

    [feat] : Implement result Information array

---
+++ js/info.js
@@ -0,0 +1,37 @@
+var resultInformation = [
+    {
+        "id" : 1,
+        "title": "<p>완전 럭키비키잖아? 🍀</p>",
+        "resultImg" : "장원영.png",
+        "name" : "장원영",
+        "tag" : ["#럭키비키", "#긍정적 사고", "#원영적 사고"],
+        "original" : ["완전 럭키비키잖아?🍀의 어원", "Lucky와 장원영의 영어 이름 Vicky를" ,"연달아 쓴 일종의 펀치라인이자 언어유희"],
+        "similarPersonImg" : ["장원영.png", "장원영.png"], 
+        "similarPerson" : ["하니", "해원"],
+        "similarTitle" : ["아르켜줄게", "외모체크!!"], 
+    },
+
+    {
+        "id" : 2,
+        "title": "<p>아르켜줄게</p>",
+        "resultImg" : "장원영.png",
+        "name" : "하니",
+        "tag" : ["#럭키비키", "#긍정적 사고", "#원영적 사고"],
+        "original" : ["아르켜줄게의 어원", "bubble gum 뮤비 때 혜인이 말한" ,"“오늘은 내가 비눗방울을 만드는 법을 아르켜줄게”에서 유래한 밈"],
+        "similarPersonImg" : ["장원영.png", "장원영.png"], 
+        "similarPerson" : ["장원영", "해원"],
+        "similarTitle" : ["럭키비키자나~", "외모체크!!"], 
+    },
+
+    {
+        "id" : 3,
+        "title": "<p>외모체크!!</p>",
+        "resultImg" : "장원영.png",
+        "name" : "오해원",
+        "tag" : ["#외모체크", "#자신감", "#긍정적인 영향"],
+        "original" : ["외모체크!!의 어원", "Lucky와 장원영의 영어 이름 Vicky를" ,"연달아 쓴 일종의 펀치라인이자 언어유희"],
+        "similarPersonImg" : ["장원영.png", "장원영.png"], 
+        "similarPerson" : ["하니", "장원영"],
+        "similarTitle" : ["아르켜줄게", "럭키비키자나~"], 
+    },
+]
