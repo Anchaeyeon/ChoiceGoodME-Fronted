96441903cc2a1a2e7a9faab59583377f40282d5e
오지은 <w2317@e-mirim.hs.kr>
2024-10-19 05:28:28 +0900

    [fix] : home link connect

--- js/home.js
+++ js/home.js
@@ -11,5 +11,11 @@ document.querySelector('.logo-image').addEventListener('click', function() {
     window.location.href = 'login.html';  
 });
 
+document.querySelector('.search-btn').addEventListener('click', function(){
+    const inputBox = document.getElementById('inputbox');
+    const searchInfo = inputBox.value;  // Use value instead of textContent for input elements
+    localStorage.setItem('searchInfo', searchInfo);
+    window.location.href = 'info.html';
+});
 
 
